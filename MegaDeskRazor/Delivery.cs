e2880df47170b4cfe1173a7eabad2dcf9c42fb8e
kosorio2 <oso16001@byui.edu>
2021-06-16 14:13:53 -0600

    Add project files.

---
+++ MegaDeskRazor/Delivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskRazor
+{
+    public class Delivery
+    {
+        public int DeliveryId { get; set; }
+
+        public string DeliveryType { get; set; }
+
+        public decimal LessThan1000 { get; set; }
+
+        public decimal Between100And2000 { get; set; }
+
+        public decimal GreaterThan2000 { get; set; }
+    }
+}
---
+++ MegaDeskRazor/DesktopMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskRazor
+{
+    public class DesktopMaterial
+    {
+        public int DesktopMaterialId { get; set; }
+
+        public string DesktopMaterialName { get; set; } // This will hold the data, like laminate, oak, etc. 
+
+        public decimal DesktopMaterialPrice { get; set; }
+    }
+}
